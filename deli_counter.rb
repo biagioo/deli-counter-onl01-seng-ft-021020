5c4d29817b35e75bbb4adedebbf02afea89cf547
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-12 15:57:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+require 'pry'
+katz_deli = [ ] 
+
+def line(katz_deli) 
+  new_line = []
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  elsif katz_deli.length >= 1 
+    katz_deli.each_with_index do |name, index| 
+      line_position = index + 1
+      new_line.push("#{line_position}. #{name} ")
+    end
+    puts "The line is currently: #{new_line.join}".chomp(" ")
+  end
+end
+
+def take_a_number(katz_deli, name)
+ line = []
+ katz_deli.push(name)
+ puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+# katz_deli.each_with_index do |name, index| 
+#   line_pos = index + 1 
+#   line.push("Welcome, #{name}. You are number #{line_pos} in line.")
+#   binding.pry
+# end 
+ puts "#{line.join}"
+end
+
+def now_serving 
+  
+end
